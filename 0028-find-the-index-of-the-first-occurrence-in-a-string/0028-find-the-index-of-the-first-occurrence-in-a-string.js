16ad0b03199d25ba974dcf1bbe8ad92a38228663
Samuel Ayine Yismaw <samiayine3@gmail.com>
2026-01-04 14:19:37 +0300

    Time: 0 ms (100%), Space: 53.4 MB (88.56%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if(needle.length === 0) return 0;
+
+    for(let i = 0; i<= haystack.length - needle.length; i++){
+        let j = 0;
+        for(; j<needle.length;j++){
+            if(haystack[i + j] !== needle[j]) break;
+        }
+        if(j === needle.length) return i;
+    }
+    return -1;
+};
