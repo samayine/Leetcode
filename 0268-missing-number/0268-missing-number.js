043fd1261c279ccee77a76a7b41c3ce80b909c0b
Samuel Ayine Yismaw <samiayine3@gmail.com>
2026-01-28 22:05:39 +0300

    Time: 18 ms (18.69%), Space: 56.6 MB (55.06%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    nums.sort((a,b) => a - b);
+    const n = nums.length;
+    for(let i=0; i<n;i++){
+        if(nums[i] !== i){
+            return i;
+        }
+
+    }
+    return n;
+
+};
