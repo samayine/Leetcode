cf9f0833b3dbd06b66125064fbd63af65e127aed
Samuel Ayine Yismaw <samiayine3@gmail.com>
2026-02-02 22:49:45 +0300

    Time: 11 ms (51.06%), Space: 60.3 MB (72.42%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -3,9 +3,7 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    const st = s.split(' ');
-    for(let i=0; i<st.length; i++){
-        st[i] = st[i].split('').reverse().join('');
-    }
-    return st.join(' ');
+    return s.split(' ')
+    .map(word => word.split('').reverse().join(''))
+    .join(' ');
 };
