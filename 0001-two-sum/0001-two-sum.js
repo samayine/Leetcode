c2a3be142334e8de3ece070f12f004677422f548
Samuel Ayine <samiayine3@gmail.com>
2026-02-06 00:42:55 +0300

    Time: 33 ms (31.23%), Space: 54.4 MB (82.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,12 +4,9 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let [i, el] of nums.entries()){
-        for(let j=i+1; j<nums.length;j++){
-            if(target === nums[j] + el){
-                return [i, j]
-            }
+    for(let i=0; i<nums.length; i++){
+        for(let j= i+1; j<nums.length; j++){
+            if(nums[i]+nums[j] == target) return [i, j];
         }
     }
-    return [];
 };
