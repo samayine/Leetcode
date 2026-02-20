f5264905c0e20d8bd3b64195b04fbff5016f348b
Samuel Ayine <samiayine3@gmail.com>
2026-02-20 16:22:02 +0300

    Time: 38 ms (30.15%), Space: 61.8 MB (20.99%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    let freq = new Map();
+
+    for(let ch of s){
+        freq.set(ch, (freq.get(ch) || 0) + 1);
+    }
+
+    for(let i=0; i<s.length; i++){
+        if(freq.get(s[i]) === 1) return i;
+    }
+    return -1;
+};
