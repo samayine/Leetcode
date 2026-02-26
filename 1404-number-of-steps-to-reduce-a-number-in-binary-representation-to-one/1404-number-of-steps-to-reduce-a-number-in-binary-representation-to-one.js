10e548e14214e1a6ff0b34b0e75eb7afaced79d1
Samuel Ayine <samiayine3@gmail.com>
2026-02-26 21:27:35 +0300

    Time: 0 ms (100%), Space: 54.1 MB (56.25%) - LeetHub

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numSteps = function(s) {
+    let steps = 0;
+    let carry = 0;
+
+    for (let i = s.length - 1; i > 0; i--) {
+        let bit = Number(s[i]) + carry;
+
+        if (bit === 1) {
+            steps += 2;
+            carry = 1;
+        } else {
+            steps += 1;
+        }
+    }
+
+    return steps + carry;
+};
