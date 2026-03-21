8f2ea470cfb11ca736d786487e6c93d0f09d7891
Samuel Ayine <samiayine3@gmail.com>
2026-03-21 23:49:19 +0300

    Time: 44 ms (82.74%), Space: 58.5 MB (15.74%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function(nums, target) {
+    const dp = new Array(target + 1).fill(0);
+    
+    dp[0] = 1;
+    
+    for (let i = 1; i <= target; i++) {
+        for (let num of nums) {
+            if (i - num >= 0) {
+                dp[i] += dp[i - num];
+            }
+        }
+    }
+    
+    return dp[target];
+};
