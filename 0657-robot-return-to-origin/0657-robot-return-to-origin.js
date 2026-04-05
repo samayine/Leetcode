32b1edb13390e058c37fc7e015a0ecc9f0190619
Samuel Ayine <samiayine3@gmail.com>
2026-04-05 22:31:31 +0300

    Time: 8 ms (36.79%), Space: 57.6 MB (23.08%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+    let x = 0;
+    let y = 0;
+
+    for(let move of moves){
+        if(move == 'U') y++;
+        else if(move == 'D') y--;
+        else if(move == 'R') x++;
+        else if(move == 'L') x--;
+    }
+
+    return x === 0 && y === 0;
+};
