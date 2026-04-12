324e3416e8e5db38cdcaabc1bef551accab550a1
Samuel Ayine <samiayine3@gmail.com>
2026-04-12 22:41:29 +0300

    Time: 0 ms (100%), Space: 54.1 MB (42.32%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -7,6 +7,5 @@ var arrayStringsAreEqual = function(word1, word2) {
     const st1 = word1.join('');
     const st2 = word2.join('');
 
-    if(st1 === st2) return true;
-    return false;
+    return st1 === st2;
 };
