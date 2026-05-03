25c54bdc63b9170908ef6628129c6b496251617b
Samuel Ayine <samiayine3@gmail.com>
2026-05-03 20:33:14 +0300

    Time: 0 ms (100%), Space: 62.7 MB (92.21%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    let n = nums.length;
+    k = k % n;
+    let rotated = nums.splice(n-k);
+    nums.unshift(...rotated)
+}
