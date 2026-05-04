90b1761d0f829ade85031750681bc7d70f43315f
Samuel Ayine <samiayine3@gmail.com>
2026-05-05 00:19:49 +0300

    Time: 28 ms (80.04%), Space: 36.6 MB (43.22%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,10 +1,10 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        seen = {}  # Dictionary to store last seen index of each value
+        seen = {}
         
         for i, num in enumerate(nums):
             if num in seen and i - seen[num] <= k:
                 return True
-            seen[num] = i  # Update/record the current index
+            seen[num] = i
         
         return False
